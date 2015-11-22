37c66a2fa2729897140cff4a68f77f15acf62831
1063304800@qq.com <1063304800@qq.com>
2015-11-22 09:31:56 +0800

    zengjie

--- temp/src/com/HelloWorld.java
+++ temp/src/com/HelloWorld.java
@@ -4,4 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("master helloworld!");
 	}
+	
+	
+	public void show(){
+		System.out.println("zengjie helloworld");
+	}
 }
